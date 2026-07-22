8962a9198fb233ef23f1a80eaa9c5e6c92093e3b
nurra-sh <shokanovnurrakhman@gmail.com>
2026-07-22 14:13:44 +0500

    final base commit

--- src/modules/passengers/ui/PassengerInfoCard.tsx
+++ src/modules/passengers/ui/PassengerInfoCard.tsx
@@ -1,17 +1,18 @@
 import { Card, DatePicker, Form, Input } from "antd";
-import { useForm, type FormProps } from "antd/es/form/Form";
+import { type FormProps } from "antd/es/form/Form";
 import classes from './passengers.module.css';
 import { PatternFormat } from "react-number-format";
+import type { Dayjs } from "dayjs";
 
-interface PassengerInfoFormValues {
+export interface PassengerInfoFormValues {
     fullname: string;
     phoneNumber: string;
     email: string;
-    dateOfBirth: string;
+    dateOfBirth: Dayjs;
 }
 
 export default function PassengerInfoCard({ index }: { index: number }) {
-    const [form] = useForm<PassengerInfoFormValues>()
+    const baseName: (string | number)[] = ['passengers', index];
 
     function onFinish(values: PassengerInfoFormValues) {
         console.log('Success:', values)
@@ -22,11 +23,11 @@ export default function PassengerInfoCard({ index }: { index: number }) {
 
     return (
         <Card size="small" title={`Passenger Info ${index + 1}`}>
-            <Form<PassengerInfoFormValues> className={classes.passengerInfoForm} size='middle' form={form} name="passengerInfoForm" onFinish={onFinish} onFinishFailed={onFinishFailed} layout="vertical">
-                <Form.Item name="fullname" label="Full name" rules={[{ required: true, message: 'Please enter your full name' }]}>
+            <div className={classes.passengerInfoForm}>
+                <Form.Item name={[...baseName, 'fullname']} label="Full name" rules={[{ required: true, message: 'Please enter your full name' }]}>
                     <Input variant="filled" placeholder="Enter your full name" />
                 </Form.Item>
-                <Form.Item name="phoneNumber" label="Phone number" rules={[{ required: true, message: 'Please enter your phone number' }]}>
+                <Form.Item name={[...baseName, 'phoneNumber']} label="Phone number" rules={[{ required: true, message: 'Please enter your phone number' }]}>
                     <PatternFormat
                         format='+7 (###) ### ## ##'
                         mask='_'
@@ -36,13 +37,14 @@ export default function PassengerInfoCard({ index }: { index: number }) {
                         variant="filled"
                     />
                 </Form.Item>
-                <Form.Item name="email" label="Email" rules={[{ required: true, message: 'Please enter your email' }]}>
+                <Form.Item name={[...baseName, 'email']} label="Email" rules={[{ required: true, message: 'Please enter your email' }]}>
                     <Input type='email' variant="filled" placeholder="Enter your email" />
                 </Form.Item>
-                <Form.Item name="dateOfBirth" label="Date of birth" rules={[{ required: true, message: 'Please enter your date of birth' }]}>
+                <Form.Item name={[...baseName, 'dateOfBirth']} label="Date of birth" rules={[{ required: true, message: 'Please enter your date of birth' }]}>
                     <DatePicker style={{ width: '100%' }} variant="filled" placeholder="Enter your date of birth" />
                 </Form.Item>
-            </Form>
+            </div>
+
         </Card>
     )
 }
--- src/pages/buy-tickets/BuyTicketsPage.tsx
+++ src/pages/buy-tickets/BuyTicketsPage.tsx
@@ -1,25 +1,190 @@
-import { Flex, Typography } from "antd"
+import PassengerInfoCard, { type PassengerInfoFormValues } from "../../modules/passengers/ui/PassengerInfoCard"
 import useTrainsStore from "../../modules/trains/store/useTrainsStore"
 import TrainCard from "../../modules/trains/ui/TrainCard"
-import classes from './BuyTIcketsPage.module.css'
-import PassengerInfoCard from "../../modules/passengers/ui/PassengerInfoCard"
+import classes from './BuyTicketsPage.module.css'
+import { Button, Card, Flex, Form, Input, Tag, Typography } from "antd"
+import food1Image from '../../assets/images/food1.webp'
+import food2Image from '../../assets/images/food2.jpg'
+import food3Image from '../../assets/images/food3.jpg'
+import { BiSolidDiscount } from "react-icons/bi"
+
+interface TicketFormValues {
+    passengers: PassengerInfoFormValues[];
+    food: FoodInfo[];
+    promoCode: string;
+    extraBaggage: boolean;
+}
+
+interface FoodInfo {
+    id: number;
+    title: string;
+    price: string;
+    image: string;
+    isAdded: boolean;
+}
+
+// name="passenger 0 fullname"
+// name="passenger 0 phoneNumber"
+// name="passenger 0 email"
+// name="passenger 0 dateOfBirth"
+
+// name="passenger 1 fullname"
+// name="passenger 1 phoneNumber"
+// name="passenger 1 email"
+// name="passenger 1 dateOfBirth"
+
+const foodCardsInfo: FoodInfo[] = [
+    {
+        id: 1,
+        title: "Пельмени",
+        price: "$12",
+        image: food1Image,
+        isAdded: false,
+    },
+    {
+        id: 2,
+        title: "Бешбармак",
+        price: "$20",
+        image: food2Image,
+        isAdded: false,
+    },
+    {
+        id: 3,
+        title: "Борщ",
+        price: "$15",
+        image: food3Image,
+        isAdded: false,
+    }
+]
+
+const offersInfo: OffersInfo[] = [
+    {
+        id: 1,
+        title: "50% off up to $100 | Use code BOOKNOW",
+        percentage: 50,
+        limit: 100,
+        code: "BOOKNOW",
+    },
+    {
+        id: 2,
+        title: "20% off | Use code FIRSTTIME",
+        percentage: 20,
+        code: "FIRSTTIME",
+    }
+]
+
+interface OffersInfo {
+    id: number;
+    title: string;
+    percentage: number;
+    limit?: number;
+    code: string;
+}
 
 export default function BuyTicketsPage() {
-  const { selectedTrain, selectedInfo } = useTrainsStore()
+    const { selectedTrain, selectedInfo } = useTrainsStore()
+    const [form] = Form.useForm<TicketFormValues>()
 
-  return (
-    <div>
-      <Typography.Title className={classes.title}>Review your booking</Typography.Title>
+    const foodState = Form.useWatch('food', { form, preserve: true }) ?? [];
+    const extraBaggageState = Form.useWatch('extraBaggage', { form, preserve: true }) ?? false;
 
-      <div style={{ marginBottom: '50px' }}>
-        {selectedTrain && <TrainCard segment={selectedTrain} />}
-      </div>
+    function handleAddFood(id: number) {
+        const nextFood = foodState.map((f: FoodInfo) =>
+            f.id === id ? { ...f, isAdded: true } : f
+        );
+        form.setFieldsValue({ food: nextFood });
+    }
 
-      <Flex vertical gap='medium'>
-        {Array.from({ length: selectedInfo?.numberOfPassengers ?? 0 }).map((_, index: number) => (
-          <PassengerInfoCard key={index} index={index} />
-        ))}
-      </Flex>
-    </div>
-  )
+    function handleRemoveFood(id: number) {
+        const nextFood = foodState.map((f: FoodInfo) =>
+            f.id === id ? { ...f, isAdded: false } : f
+        );
+        form.setFieldsValue({ food: nextFood });
+    }
+
+    function handleApplyPromoCode(code: string) {
+        form.setFieldsValue({ promoCode: code });
+    }
+
+    function handleRemoveExtraBaggage() {
+        form.setFieldsValue({ extraBaggage: false });
+    }
+
+    function handleAddExtraBaggage() {
+        form.setFieldsValue({ extraBaggage: true });
+    }
+
+    return (
+        <div>
+            <Typography.Title className={classes.title}>Review your booking</Typography.Title>
+
+            <div style={{ marginBottom: '50px' }}>
+                {selectedTrain && <TrainCard segment={selectedTrain} />}
+            </div>
+
+            <Form<TicketFormValues>
+                form={form}
+                layout="vertical"
+                initialValues={{ passengers: [], food: foodCardsInfo, promoCode: '', extraBaggage: false }}
+            >
+                <Flex vertical gap="medium">
+                    {Array.from({ length: selectedInfo?.numberOfPassengers ?? 0 }).map((_, index) => (
+                        <PassengerInfoCard key={index} index={index} />
+                    ))}
+                </Flex>
+
+
+                <Flex gap="32px" wrap="wrap" style={{ marginTop: '50px' }}>
+                    {foodCardsInfo.map((food, index) => (
+                        <Card
+                            key={index}
+                            classNames={{ body: classes.foodCard }}
+                            hoverable
+                            style={{ flex: '1 1 100px' }}
+                            cover={
+                                <img
+                                    draggable={false}
+                                    height={150}
+                                    alt={food.title}
+                                    src={food.image}
+                                    className={classes.foodImage}
+                                />
+                            }
+                        >
+                            <Typography.Title className={classes.foodTitle} level={2}>{food.title}</Typography.Title>
+                            <Tag color="blue" className={classes.foodPrice}>{food.price}</Tag>
+                            {foodState.find((f) => f.id === food.id)?.isAdded && <Button variant="outlined" color="red" className={classes.foodButton} onClick={() => handleRemoveFood(food.id)}>Remove</Button>}
+                            {!foodState.find((f) => f.id === food.id)?.isAdded && <Button variant="outlined" color="geekblue" type="primary" className={classes.foodButton} onClick={() => handleAddFood(food.id)}>Add to ticket</Button>}
+                        </Card>
+                    ))}
+                </Flex>
+                <div className={classes.offersCard}>
+                    <Typography.Title level={2} className={classes.offersTitle}>Offers</Typography.Title>
+
+                    {offersInfo.map((offer) => (
+                        <Flex align="center" justify="space-between" style={{ marginBottom: '10px' }}>
+                            <Flex align="center" gap="10px">
+                                <BiSolidDiscount size={24} color="#9254de" /> <Typography.Title level={3} className={classes.offersText}>{offer.title}</Typography.Title>
+                            </Flex>
+                            <Button variant="text" color="purple" type="primary" className={classes.offersButton} onClick={() => handleApplyPromoCode(offer.code)}>Apply</Button>
+                        </Flex>
+                    ))}
+                </div>
+
+                <Flex align="center" gap="32px">
+                    <Card style={{ flex: 1 }}>
+                        <Typography.Title className={classes.codeTitle} level={2}>Apply Code</Typography.Title>
+                        <Form.Item name="promoCode" rules={[{ required: true, message: 'Please enter a promo code' }]}>
+                            <Input variant="underlined" placeholder="Enter code" />
+                        </Form.Item>
+                    </Card>
+                    <Card style={{ flex: 1 }}>
+                        <Typography.Title className={classes.extraBaggageTitle} level={2}>Extra Baggage</Typography.Title>
+                        {extraBaggageState && <Button variant="outlined" color="red" type="primary" onClick={handleRemoveExtraBaggage}>Remove from ticket</Button>}
+                        {!extraBaggageState && <Button variant="outlined" color="purple" type="primary" onClick={handleAddExtraBaggage}>Add to ticket</Button>}
+                    </Card>
+                </Flex>
+            </Form>
+        </div>
+    )
 }
